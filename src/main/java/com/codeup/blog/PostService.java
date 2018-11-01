6174ae099db61274abbdf4de03d3fbf6b3872920
Daniel Fryar <daniel@codeup.com>
2018-11-01 16:59:28 -0500

    Done with edit and delete methods, need to call them from controller

--- src/main/java/com/codeup/blog/PostService.java
+++ src/main/java/com/codeup/blog/PostService.java
@@ -1,5 +1,6 @@
 package com.codeup.blog;
 
+import com.codeup.blog.services.PostRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -7,26 +8,38 @@ import java.util.List;
 
 @Service
 public class PostService {
-    private List<Post> posts;
+//    private List<Post> posts;
+    private PostRepository postRepo;
 
-    public PostService() {
-        posts = new ArrayList<>();
-
-        posts.add(new Post("Halloween", "Michael Myers"));
-        posts.add(new Post("Friday 13th", "Jason Voorhees"));
-        posts.add(new Post("Nightmare on Elm Street", "Freddy Krueger"));
-        posts.add(new Post("IT", "Pennywise"));
-        posts.add(new Post("Saw", "Jigsaw"));
-        posts.add(new Post("Charlie Brown", "The Great Pumpkin"));
-        posts.add(new Post("Poltergeist", "They're heeeeerre"));
+    public PostService(PostRepository postRepo) {
+        this.postRepo = postRepo;
+        //        posts = new ArrayList<>();
+//
+//        posts.add(new Post("Halloween", "Michael Myers"));
+//        posts.add(new Post("Friday 13th", "Jason Voorhees"));
+//        posts.add(new Post("Nightmare on Elm Street", "Freddy Krueger"));
+//        posts.add(new Post("IT", "Pennywise"));
+//        posts.add(new Post("Saw", "Jigsaw"));
+//        posts.add(new Post("Charlie Brown", "The Great Pumpkin"));
+//        posts.add(new Post("Poltergeist", "They're heeeeerre"));
     }
 
-    public List<Post> findAll() {
-        return posts;
+    public Iterable<Post> findAll() {
+
+        return postRepo.findAll();
     }
 
-    public Post findOne(int id) {
-        return posts.get(id - 1);
+    public Post findOne(long id) {
+
+        return postRepo.findOne(id);
+    }
+
+    public Post edit(Post post) {
+        return postRepo.save(post);
+    }
+
+    public void delete(long id) {
+        postRepo.delete(id);
     }
 
 }
