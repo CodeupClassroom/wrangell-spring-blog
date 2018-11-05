5150158ebbba45d701feec18851f6af29994c278
fer <fernandoza@gmail.com>
2018-11-05 11:18:08 -0600

    feat: create a form to register and save it

---
+++ src/main/java/com/codeup/blog/controllers/UserController.java
@@ -0,0 +1,37 @@
+package com.codeup.blog.controllers;
+
+import com.codeup.blog.models.User;
+import com.codeup.blog.services.UserRepo;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    private UserRepo usersRepo;
+    private PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepo users, PasswordEncoder passwordEncoder) {
+        this.usersRepo = users;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/sign-up")
+    public String showSignupForm(Model model){
+        model.addAttribute("user", new User());
+        return "users/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String saveUser(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        usersRepo.save(user);
+        return "redirect:/login";
+    }
+
+}
